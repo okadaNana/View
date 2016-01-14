9a46a731c5713e92f193495d6c13c9844bca425d
owenmike <xiaotu.dev@gmail.com>
2016-01-14 21:04:49 +0800

    RadarView

--- app/src/main/java/com/owen/view/views/RadarView.java
+++ app/src/main/java/com/owen/view/views/RadarView.java
@@ -14,8 +14,35 @@ import android.view.View;
  */
 public class RadarView extends View {
 
-    private Paint mPaint;
-    private float mAngel = (float) (2 * Math.PI / 6);
+    /** 绘制多边形的画笔 */
+    private Paint mPaintPolygon;
+    /** 绘制直线的画笔 */
+    private Paint mPaintLine;
+    /** 绘制分值区的画笔 */
+    private Paint mPaintRegion;
+
+    /**
+     * 雷达的边数
+     */
+    private static final int NUM_OF_SIDES = 6;
+    /** 角度 */
+    private float mAngel = (float) (2 * Math.PI / NUM_OF_SIDES);
+
+    /** 多边形的绘制路径 */
+    private Path mPathPolygon;
+    /** 多边形的起始半径 */
+    private float mStartRadius = 30F;
+
+    /** 直线的绘制路径 */
+    private Path mPathLine;
+
+    /** 分值区的绘制路径 */
+    private Path mPathRegion;
+
+    /** 中心点 x 轴坐标*/
+    private int mCenterX;
+    /** 中心点 y 轴坐标 */
+    private int mCenterY;
 
     public RadarView(Context context) {
         this(context, null);
@@ -27,59 +54,44 @@ public class RadarView extends View {
     }
 
     private void init() {
-        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setColor(Color.BLACK);
-        mPaint.setStrokeWidth(2F);
+        mPaintPolygon = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintPolygon.setStyle(Paint.Style.STROKE);
+        mPaintPolygon.setColor(Color.BLACK);
+        mPaintPolygon.setStrokeWidth(1F);
+
+        mPaintLine = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintLine.setStyle(Paint.Style.STROKE);
+        mPaintLine.setColor(Color.GRAY);
+        mPaintLine.setStrokeWidth(1F);
+
+        mPaintRegion = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintRegion.setStyle(Paint.Style.FILL_AND_STROKE);
+        mPaintRegion.setColor(Color.parseColor("#7f33b5e5"));
+        mPaintRegion.setStrokeWidth(1F);
+
+        mPathPolygon = new Path();
+        mPathLine = new Path();
+        mPathRegion = new Path();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        mCenterX = getWidth() / 2;
+        mCenterY = getHeight() / 2;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawColor(Color.GRAY);
-        canvas.translate(getWidth() / 2, getHeight() / 2);
-
         drawPolygon(canvas);
         drawLine(canvas);
         drawRegion(canvas);
     }
 
     /**
-     * 绘制多边形
+     * 绘制分值区
      */
-    private void drawPolygon(Canvas canvas) {
-        Path mPath = new Path();
-        float r = 30F;
-
-        for (int k = 0; k < 5; k++) {
-            float radius = r * (1 + k);
-
-            mPath.moveTo(radius, 0);
-            for (int i = 1; i < 6; i++) {
-                float x = (float) (Math.cos(mAngel * i) * radius);
-                float y = (float) (Math.sin(mAngel * i) * radius);
-                mPath.lineTo(x, y);
-            }
-            mPath.close();
-
-            canvas.drawPath(mPath, mPaint);
-        }
-    }
-
-    private void drawLine(Canvas canvas) {
-        Path path = new Path();
-
-        for (int i = 0; i < 6; i++) {
-            path.reset();
-
-            path.moveTo(0, 0);
-            float x = (float) (Math.cos(mAngel * i) * 150);
-            float y = (float) (Math.sin(mAngel * i) * 150);
-            path.lineTo(x, y);
-
-            canvas.drawPath(path, mPaint);
-        }
-    }
-
     private void drawRegion(Canvas canvas) {
         float[] percents = new float[]{
                 0.4F,
@@ -89,28 +101,65 @@ public class RadarView extends View {
                 0.6F,
                 0.9F
         };
-        Paint paint = new Paint();
-        paint.setColor(Color.BLUE);
-        paint.setStyle(Paint.Style.FILL_AND_STROKE);
-        paint.setStrokeWidth(2F);
-        paint.setAlpha(127);
 
-        Path mPath = new Path();
+        float radius = mStartRadius * 5;
 
-        float radius = 150;
+        for (int i = 0; i < NUM_OF_SIDES; i++) {
+            float x = (float) (Math.cos(mAngel * i) * radius * percents[i]);
+            float y = (float) (Math.sin(mAngel * i) * radius * percents[i]);
 
-        for (int i = 0; i < 6; i++) {
-            float x = (float) (Math.cos(mAngel * i) * radius) * percents[i];
-            float y = (float) (Math.sin(mAngel * i) * radius) * percents[i];
             if (i == 0) {
-                mPath.moveTo(x, 0);
+                mPathRegion.moveTo(mCenterX + x, mCenterY);
             } else {
-                mPath.lineTo(x, y);
+                mPathRegion.lineTo(mCenterX + x, mCenterY + y);
             }
         }
-        mPath.close();
 
-        canvas.drawPath(mPath, paint);
+        mPathRegion.close();
+
+        canvas.drawPath(mPathRegion, mPaintRegion);
+    }
+
+    /**
+     * 绘制直线
+     */
+    private void drawLine(Canvas canvas) {
+        float radius = mStartRadius * 5;
+
+        for (int i = 0; i < NUM_OF_SIDES; i++) {
+            mPathLine.moveTo(mCenterX, mCenterY);
+
+            float x = (float) (mCenterX + Math.cos(mAngel * i) * radius);
+            float y = (float) (mCenterY + Math.sin(mAngel * i) * radius);
+
+            mPathLine.lineTo(x, y);
+
+            canvas.drawPath(mPathLine, mPaintLine);
+        }
+    }
+
+    /**
+     * 绘制多边形
+     */
+    private void drawPolygon(Canvas canvas) {
+        for (int count = 1; count <= 5; count++) {
+            float newRadius = mStartRadius * count;
+
+            mPathPolygon.moveTo(mCenterX + newRadius, mCenterY);
+
+            for (int i = 1; i < NUM_OF_SIDES; i++) {
+                float x = (float) (mCenterX + Math.cos(mAngel * i) * newRadius);
+                float y = (float) (mCenterY + Math.sin(mAngel * i) * newRadius);
+
+                mPathPolygon.lineTo(x, y);
+            }
+
+            mPathPolygon.close();
+
+            canvas.drawPath(mPathPolygon, mPaintPolygon);
+
+            mPathPolygon.reset();
+        }
     }
 
 }
